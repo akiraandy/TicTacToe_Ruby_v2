cee92ce4f109f5a43cdc9d1172e885813b29393a
Tyler Decker <tyler.decker@gmail.com>
2016-04-15 09:31:17 -0500

    Created a Ai_Basic class.
    This is just a basic AI, it tries to play a winning move if it can. Then tries to block. If it can’t do either of those it plays a random move.

---
+++ lib/ai_basic.rb
@@ -0,0 +1,40 @@
+require 'game_rules'
+
+class Ai_Basic
+  def initialize
+    @rules = Game_Rules.new
+  end
+
+  def play_move(game_state)
+    return 0 if game_state.game_board.empty?
+    move = play_to_win(game_state)
+    return move if !move.nil?
+    move = play_to_block(game_state)
+    return move if !move.nil?
+    get_random_move(game_state)
+  end
+
+  def play_to_win(game_state)
+    moves = game_state.game_board.available_moves
+    moves.each do |move|
+      temp = game_state.deep_copy
+      temp.game_board.spaces[move] = game_state.player
+      return move if @rules.game_status(temp.game_board) == game_state.player
+    end
+    nil
+  end
+
+  def play_to_block(game_state)
+    game_state.switch_turns
+    move = play_to_win(game_state)
+    game_state.switch_turns
+    move
+  end
+
+  def get_random_move(game_state)
+    moves = game_state.game_board.available_moves
+    i = rand(moves.size)
+    moves[i]
+  end
+
+end
---
+++ spec/ai_basic_spec.rb
@@ -0,0 +1,33 @@
+require 'ai_basic'
+require 'game_io'
+require 'game_board'
+require 'game_state'
+
+describe Ai_Basic do
+
+  let (:test_ai) { Ai_Basic.new() }
+  let (:test_state) { Game_State.new(Game_Board.new , 'X', 'O') }
+
+  describe "playing moves " do
+    it "returns 0 (top corner) on empty board " do
+      expect(test_ai.play_move(test_state)).to eq(0)
+    end
+
+    it "plays winning move if it can " do
+      test_state.game_board.spaces[4] = 'X'
+      test_state.game_board.spaces[3] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(5)
+    end
+
+    it "blocks opponent from winning if it can " do
+      test_state.game_board.spaces[4] = 'O'
+      test_state.game_board.spaces[3] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(5)
+    end
+
+    it "returns random move when can't win or block " do
+      test_state.game_board.spaces[2] = 'O'
+      expect(test_ai.play_move(test_state).class).to eq(Fixnum)
+    end
+  end
+end
