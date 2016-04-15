c484c80c2dbe97d3524a079ca0706f4f16ceadab
Tyler Decker <tyler.decker@gmail.com>
2016-04-15 09:25:43 -0500

    added switch_turns and deep_copy

--- lib/game_state.rb
+++ lib/game_state.rb
@@ -1,8 +1,19 @@
 class Game_State
   attr_accessor :game_board, :player, :opponent
   def initialize(game_board, player_mark, opponent_mark)
-    @game_board = game_board.dup
+    @game_board = game_board.deep_copy
     @player = player_mark
     @opponent = opponent_mark
+  end
+
+  def switch_turns
+    temp = @player
+    @player = @opponent
+    @opponent = @player
+  end
+
+  def deep_copy
+    temp_board = game_board.deep_copy
+    Game_State.new(temp_board, @player, @opponent)
   end
 end
--- spec/game_state_spec.rb
+++ spec/game_state_spec.rb
@@ -9,4 +9,12 @@ describe Game_State do
       test_board.clear('x')
       expect(test_state.game_board.spaces[0]).to eq(' ')
   end
+
+  it "Swaps players " do
+    test_state = Game_State.new(test_board, 'X', 'O')
+    test_state.switch_turns
+    expect(test_state.player).to eq('O')
+  end
+
+
 end
