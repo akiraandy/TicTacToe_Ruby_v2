998aadc2d96e9552876293f9fb3417214bf0624e
Tyler Decker <tyler.decker@gmail.com>
2016-04-18 09:11:39 -0500

    Updated the print_board method.

--- lib/game_io.rb
+++ lib/game_io.rb
@@ -1,13 +1,22 @@
-require 'game_board'
+
 class Game_Io
 
   def print_board(spaces, grid_size)
     rtn_string = "\n"
     spaces.each.with_index do |val, i|
-      rtn_string << val
+      if val == ' '
+        rtn_string << '-'
+      else
+        rtn_string << val
+      end
       rtn_string << "\n" if ((i + 1) % grid_size == 0)
     end
+    puts rtn_string
     rtn_string
+  end
+
+  def clear_screen
+    system("clear")
   end
 
   def print_message(print_string)
