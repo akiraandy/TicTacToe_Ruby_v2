6a938b8a656d428b3d8db0cc1b5f5e92c68e3324
Tyler Decker <tyler.decker@gmail.com>
2016-04-18 15:02:17 -0500

    Redid the BasicAi class using proper TTD.

--- lib/ai_basic.rb
+++ lib/ai_basic.rb
@@ -3,37 +3,108 @@ require 'game_rules'
 class AiBasic
 
   def play_move(game_state)
-    return 0 if game_state.game_board.empty?
-    move = play_to_win(game_state)
-    return move if !move.nil?
-    move = play_to_block(game_state)
-    return move if !move.nil?
-    get_random_move(game_state)
+    board = game_state.game_board
+    @spaces = game_state.game_board.spaces
+    @available_moves = game_state.game_board.available_moves
+    @player = game_state.player
+    @opponent = game_state.opponent
+
+    choice = get_corner if board.empty?
+    choice = play_to_win if choice.nil?
+    choice = play_to_block if choice.nil?
+    choice = get_corner if choice.nil?
+    choice 
   end
 
 private
 
-  def play_to_win(game_state)
-    moves = game_state.game_board.available_moves
-    moves.each do |move|
-      temp = game_state.deep_copy
-      temp.game_board.spaces[move] = game_state.player
-      rules = GameRules.new(temp.game_board)
-      return move if rules.game_status == game_state.player
+  def get_corner
+    corners = [0, 2, 6, 8]
+    available_corners =  corners & @available_moves
+    index = rand(0...available_corners.size)
+    available_corners[index]
+  end
+
+  def play_to_block
+    swap_players
+    choice = play_to_win
+    swap_players
+    choice
+  end
+
+  def swap_players
+    @player, @opponent = @opponent, @player
+  end
+
+  def play_to_win
+    if @available_moves.include?(0)
+      if @spaces[1] == @spaces[2] && @spaces[1] == @player || \
+         @spaces[3] == @spaces[6] && @spaces[3] == @player || \
+         @spaces[4] == @spaces[8] && @spaces[4] == @player
+        return 0
+      end
     end
-    nil
+
+    if @available_moves.include?(1)
+      if @spaces[0] == @spaces[2] && @spaces[0] == @player || \
+         @spaces[4] == @spaces[7] && @spaces[4] == @player
+        return 1
+      end
+    end
+
+    if @available_moves.include?(2)
+      if @spaces[0] == @spaces[1] && @spaces[0] == @player || \
+         @spaces[5] == @spaces[8] && @spaces[5] == @player || \
+         @spaces[4] == @spaces[6] && @spaces[4] == @player
+        return 2
+      end
+    end
+
+    if @available_moves.include?(3)
+      if @spaces[4] == @spaces[5] && @spaces[5] == @player || \
+         @spaces[0] == @spaces[6] && @spaces[0] == @player
+        return 3
+      end
+    end
+
+    if @available_moves.include?(4)
+      if @spaces[3] == @spaces[5] && @spaces[5] == @player || \
+         @spaces[1] == @spaces[7] && @spaces[1] == @player || \
+         @spaces[0] == @spaces[8] && @spaces[0] == @player || \
+         @spaces[2] == @spaces[6] && @spaces[2] == @player
+        return 4
+      end
+    end
+
+    if @available_moves.include?(5)
+      if @spaces[3] == @spaces[4] && @spaces[3] == @player || \
+         @spaces[2] == @spaces[8] && @spaces[2] == @player
+        return 5
+      end
+    end
+
+    if @available_moves.include?(6)
+      if @spaces[7] == @spaces[8] && @spaces[7] == @player || \
+         @spaces[0] == @spaces[3] && @spaces[0] == @player || \
+         @spaces[2] == @spaces[4] && @spaces[2] == @player
+        return 6
+      end
+    end
+
+    if @available_moves.include?(7)
+      if @spaces[6] == @spaces[8] && @spaces[6] == @player || \
+         @spaces[1] == @spaces[4] && @spaces[1] == @player
+        return 7
+      end
+    end
+
+    if @available_moves.include?(8)
+      if @spaces[6] == @spaces[7] && @spaces[6] == @player || \
+         @spaces[2] == @spaces[5] && @spaces[2] == @player || \
+         @spaces[0] == @spaces[4] && @spaces[0] == @player
+        return 8
+      end
+    end
   end
 
-  def play_to_block(game_state)
-    game_state.switch_turns
-    move = play_to_win(game_state)
-    game_state.switch_turns
-    move
-  end
-
-  def get_random_move(game_state)
-    moves = game_state.game_board.available_moves
-    i = rand(moves.size)
-    moves[i]
-  end
 end
--- spec/ai_basic_spec.rb
+++ spec/ai_basic_spec.rb
@@ -6,27 +6,268 @@ describe AiBasic do
 
   let (:test_ai) { AiBasic.new() }
   let (:test_state) { GameState.new(GameBoard.new , 'X', 'O') }
-
-  describe "playing moves " do
+  let(:spaces) { test_state.game_board.spaces }
+  let(:corners) { [0, 2, 6, 8] }
+  describe "Plays a corner " do
     it "returns 0 (top corner) on empty board " do
+      test_for_true = corners.include?(test_ai.play_move(test_state))
+      expect(test_for_true).to eq(true)
+    end
+
+    it "returns a corner if only move played so far is center " do
+      spaces[4] = 'O'
+      test_for_true = corners.include?(test_ai.play_move(test_state))
+      expect(test_for_true).to eq(true)
+    end
+  end
+
+  describe "plays winning move if it can " do
+
+    it "returns 0 if 1 and 2 are 'X' " do
+      spaces[1] = spaces[2] = 'X'
       expect(test_ai.play_move(test_state)).to eq(0)
     end
 
-    it "plays winning move if it can " do
-      test_state.game_board.spaces[4] = 'X'
-      test_state.game_board.spaces[3] = 'X'
+    it "returns 0 if 3 and 6 are 'X' " do
+      spaces[3] = spaces[6] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(0)
+    end
+
+    it "returns 1 if 0 and 2 are 'X' " do
+      spaces[0] = spaces[2] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(1)
+    end
+
+    it "returns 1 if 4 and 7 are 'X' " do
+      spaces[4] = spaces[7] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(1)
+    end
+
+    it "returns 2 if 0 and 1 are 'X' " do
+      spaces[0] = spaces[1] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(2)
+    end
+
+    it "returns 2 if 5 and 8 are 'X' " do
+      spaces[5] = spaces[8] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(2)
+    end
+
+    it "returns 3 if 4 and 5 are 'X' " do
+      spaces[4] = spaces[5] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(3)
+    end
+
+    it "returns 3 if 0 and 6 are 'X' " do
+      spaces[0] = spaces[6] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(3)
+    end
+
+    it "returns 4 if 3 and 5 and 'X' " do
+      spaces[3] = spaces[5] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(4)
+    end
+
+    it "returns 4 if 1 and 7 and 'X' " do
+      spaces[1] = spaces[7] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(4)
+    end
+
+    it "returns 5 if 3 and 4 and 'X' " do
+      spaces[3] = spaces[4] = 'X'
       expect(test_ai.play_move(test_state)).to eq(5)
     end
 
-    it "blocks opponent from winning if it can " do
-      test_state.game_board.spaces[4] = 'O'
-      test_state.game_board.spaces[3] = 'O'
+    it "returns 5 if 2 and 8 and 'X' " do
+      spaces[2] = spaces[8] = 'X'
       expect(test_ai.play_move(test_state)).to eq(5)
     end
 
-    it "returns random move when can't win or block " do
-      test_state.game_board.spaces[2] = 'O'
-      expect(test_ai.play_move(test_state).class).to eq(Fixnum)
+    it "returns 6 if 7 and 8 and 'X' " do
+      spaces[7] = spaces[8] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(6)
+    end
+
+    it "returns 6 if 0 and 3 and 'X' " do
+      spaces[0] = spaces[3] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(6)
+    end
+
+    it "returns 7 if 6 and 8 and 'X' " do
+      spaces[6] = spaces[8] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(7)
+    end
+
+    it "returns 7 if 1 and 4 and 'X' " do
+      spaces[1] = spaces[4] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(7)
+    end
+
+    it "returns 8 if 6 and 7 and 'X' " do
+      spaces[6] = spaces[7] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(8)
+    end
+
+    it "returns 8 if 2 and 5 and 'X' " do
+      spaces[2] = spaces[5] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(8)
+    end
+
+    it "returns 0 if 4 and 8 are 'X' " do
+      spaces[4] = spaces[8] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(0)
+    end
+
+    it "returns 2 if 4 and 6 are 'X' " do
+      spaces[4] = spaces[6] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(2)
+    end
+
+    it "returns 4 if 0 and 8 are 'X' " do
+      spaces[0] = spaces[8] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(4)
+    end
+
+    it "returns 2 if 0 and 6 are 'X' " do
+      spaces[2] = spaces[6] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(4)
+    end
+
+    it "returns 6 if 2 and 4 are 'X' " do
+      spaces[2] = spaces[4] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(6)
+    end
+
+    it "returns 8 if 0 and 4 are 'X' " do
+      spaces[0] = spaces[4] = 'X'
+      expect(test_ai.play_move(test_state)).to eq(8)
     end
   end
+
+  describe "plays blocking move if it can " do
+
+    it "returns 0 if 1 and 2 are 'X' " do
+      spaces[1] = spaces[2] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(0)
+    end
+
+    it "returns 0 if 3 and 6 are 'X' " do
+      spaces[3] = spaces[6] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(0)
+    end
+
+    it "returns 1 if 0 and 2 are 'X' " do
+      spaces[0] = spaces[2] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(1)
+    end
+
+    it "returns 1 if 4 and 7 are 'X' " do
+      spaces[4] = spaces[7] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(1)
+    end
+
+    it "returns 2 if 0 and 1 are 'X' " do
+      spaces[0] = spaces[1] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(2)
+    end
+
+    it "returns 2 if 5 and 8 are 'X' " do
+      spaces[5] = spaces[8] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(2)
+    end
+
+    it "returns 3 if 4 and 5 are 'X' " do
+      spaces[4] = spaces[5] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(3)
+    end
+
+    it "returns 3 if 0 and 6 are 'X' " do
+      spaces[0] = spaces[6] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(3)
+    end
+
+    it "returns 4 if 3 and 5 and 'X' " do
+      spaces[3] = spaces[5] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(4)
+    end
+
+    it "returns 4 if 1 and 7 and 'X' " do
+      spaces[1] = spaces[7] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(4)
+    end
+
+    it "returns 5 if 3 and 4 and 'X' " do
+      spaces[3] = spaces[4] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(5)
+    end
+
+    it "returns 5 if 2 and 8 and 'X' " do
+      spaces[2] = spaces[8] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(5)
+    end
+
+    it "returns 6 if 7 and 8 and 'X' " do
+      spaces[7] = spaces[8] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(6)
+    end
+
+    it "returns 6 if 0 and 3 and 'X' " do
+      spaces[0] = spaces[3] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(6)
+    end
+
+    it "returns 7 if 6 and 8 and 'X' " do
+      spaces[6] = spaces[8] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(7)
+    end
+
+    it "returns 7 if 1 and 4 and 'X' " do
+      spaces[1] = spaces[4] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(7)
+    end
+
+    it "returns 8 if 6 and 7 and 'X' " do
+      spaces[6] = spaces[7] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(8)
+    end
+
+    it "returns 8 if 2 and 5 and 'X' " do
+      spaces[2] = spaces[5] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(8)
+    end
+
+    it "returns 0 if 4 and 8 are 'X' " do
+      spaces[4] = spaces[8] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(0)
+    end
+
+    it "returns 2 if 4 and 6 are 'X' " do
+      spaces[4] = spaces[6] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(2)
+    end
+
+    it "returns 4 if 0 and 8 are 'X' " do
+      spaces[0] = spaces[8] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(4)
+    end
+
+    it "returns 2 if 0 and 6 are 'X' " do
+      spaces[2] = spaces[6] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(4)
+    end
+
+    it "returns 6 if 2 and 4 are 'X' " do
+      spaces[2] = spaces[4] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(6)
+    end
+
+    it "returns 8 if 0 and 4 are 'X' " do
+      spaces[0] = spaces[4] = 'O'
+      expect(test_ai.play_move(test_state)).to eq(8)
+    end
+  end
+
+
+
+
 end
