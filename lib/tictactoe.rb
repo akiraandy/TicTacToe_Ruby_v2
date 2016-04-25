cefa3a42d51ea6ec59a16a05738cfc3f679d2734
Tyler Decker <tyler.decker@gmail.com>
2016-04-25 14:46:37 -0500

    Updated TicTacToe class.

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,48 +1,31 @@
 require 'game_io'
 require 'game_board'
-require 'ai_basic'
+require 'player_ai_basic'
 require 'game_state'
 require 'game_rules'
-require 'player'
+require 'player_human'
 require 'player_manager'
-
+require 'wrapper_stdio'
+require 'game_loop'
+require 'game_screen'
 class TicTacToe
 
   attr_accessor :board
 
-  def initialize (player1 = nil, player2 = nil)
-    @io = GameIo.new
+  def initialize (game_io, player1, player2)
+
+    @io = game_io
     @players = PlayerManager.new(@io, player1, player2)
     @board = GameBoard.new
     @rules = GameRules.new(@board)
     @legend = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
     @legend_size = 3
+    @game_screen = GameScreen.new(@io, @board, @players, @rules, @legend, @legend_size)
+    @game_loop = GameLoop.new(@board, @game_screen, @rules, @players)
   end
 
-  def game_loop
-    until @rules.game_over?
-      print_screen
-      @players.play_turn(@board)
-    end
-      print_screen
-      print_end_of_game
-      true
+  def play_game
+    @game_loop.game_loop
   end
 
-  def print_end_of_game
-    if @rules.tied?
-      @io.puts_message("The Game Ended in a Tie!\n")
-    elsif @rules.winner
-      @io.puts_message("The Winner is #{@rules.winner}!\n")
-    end
-    @rules.game_over?
-  end
-
-  def print_screen
-    @io.clear_screen
-    @io.puts_message("\n Legend ")
-    @io.print_board(@legend, @legend_size)
-    @io.puts_message("\n Tic Tac Toe ")
-    @io.print_board(@board.spaces, @board.grid_size)
-  end
 end
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,51 +1,6 @@
 require 'tictactoe'
-require 'player'
-require 'game_io'
+
 
 describe TicTacToe do
-  let (:ttt) { TicTacToe.new }
-  let (:io) { GameIo.new }
-  let (:p1) { PlayerAiBasic.new('X', io) }
-  let (:p2) { PlayerAiBasic.new('O', io) }
-  let (:ttt_2) { TicTacToe.new(p1, p2)}
 
-  describe "The Game Loop " do
-    it "Should complete the game loop " do
-      expect(ttt_2.game_loop).to eq(true)
-    end
-  end
-
-  describe "Printing end of game " do
-    it "Prints tied game when the game is tied " do
-      ttt.board.spaces = ['X', 'O', "A", "b", "c", "d", "e", "f", "g"]
-      should_return = "The Game Ended in a Tie!\n"
-      expect(STDOUT).to receive(:puts).with(should_return)
-      expect(ttt.print_end_of_game).to eq(true)
-    end
-
-    it "Prints 'X' if the winner if 'X' winds " do
-      ttt.board.spaces = ['X', 'X', "X", "b", "c", "d", "e", "f", "g"]
-      should_return = "The Winner is X!\n"
-      expect(STDOUT).to receive(:puts).with(should_return)
-      expect(ttt.print_end_of_game).to eq(true)
-    end
-
-    it "Prints nothing if game is still going on " do
-      expect(ttt.print_end_of_game).to eq(false)
-    end
-  end
-
-  describe "Printing the game screeen " do
-    it "Should print Tic Tac Toe and a blank board, if no moves have been played " do
-      should_return1 = "\n Legend "
-      should_return2 = "\n012\n345\n678\n"
-      should_return3 = "\n Tic Tac Toe "
-      should_return4 = "\n---\n---\n---\n"
-      expect(STDOUT).to receive(:puts).with(should_return1)
-      expect(STDOUT).to receive(:puts).with(should_return2)
-      expect(STDOUT).to receive(:puts).with(should_return3)
-      expect(STDOUT).to receive(:puts).with(should_return4)
-      ttt.print_screen
-    end
-  end
 end
