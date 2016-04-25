98477e6b4baaa298020d5abc443af599dc61e03f
Tyler Decker <tyler.decker@gmail.com>
2016-04-25 14:02:51 -0500

    Updated test to use mock for io.

--- spec/player_human_spec.rb
+++ spec/player_human_spec.rb
@@ -1,12 +1,14 @@
-require 'player'
+require 'player_human'
 require 'game_board'
 require 'game_state'
 require 'game_io'
 
+
+
 describe PlayerHuman do
-  let(:game_io) { GameIo.new}
+  let(:game_io) { "dummy" }
   let(:test_player) { PlayerHuman.new(:X, game_io) }
-  let(:test_player2) { PlayerHuman.new(:O, game_io) } 
+  let(:test_player2) { PlayerHuman.new(:O, game_io) }
   let(:board) { GameBoard.new }
 
   it "mark should retrun 'X' if that was passed in for consturctor " do
@@ -18,71 +20,68 @@ describe PlayerHuman do
   end
 
   describe "play_move returns valid moves " do
+
     it "accpets 0 when that is a valid move " do
       state = GameState.new(board, test_player, test_player2)
-      allow(game_io.wrapper).to receive(:gets) { "0\n" }
+      allow(game_io).to receive(:get_input) { "0" }
+      allow(game_io).to receive(:puts_message) { }
       expect(test_player.play_move(state)).to eq(0)
     end
 
     it "accpets 1 when that is a valid move " do
       state = GameState.new(board, test_player, test_player2)
-      allow(game_io.wrapper).to receive(:gets) { "1\n" }
+      allow(game_io).to receive(:get_input) { "1" }
+      allow(game_io).to receive(:puts_message) { }
       expect(test_player.play_move(state)).to eq(1)
     end
 
     it "accpets 2 when that is a valid move " do
       state = GameState.new(board, test_player, test_player2)
-      allow(game_io.wrapper).to receive(:gets) { "2\n" }
+      allow(game_io).to receive(:get_input) { "2" }
+      allow(game_io).to receive(:puts_message) { }
       expect(test_player.play_move(state)).to eq(2)
     end
 
     it "accpets 3 when that is a valid move " do
       state = GameState.new(board, test_player, test_player2)
-      allow(game_io.wrapper).to receive(:gets) { "3\n" }
+      allow(game_io).to receive(:get_input) { "3" }
+      allow(game_io).to receive(:puts_message) { }
       expect(test_player.play_move(state)).to eq(3)
     end
 
     it "accpets 4 when that is a valid move " do
       state = GameState.new(board, test_player, test_player2)
-      allow(game_io.wrapper).to receive(:gets) { "4\n" }
+      allow(game_io).to receive(:get_input) { "4" }
+      allow(game_io).to receive(:puts_message) { }
       expect(test_player.play_move(state)).to eq(4)
     end
 
     it "accpets 5 when that is a valid move " do
       state = GameState.new(board, test_player, test_player2)
-      allow(game_io.wrapper).to receive(:gets) { "5\n" }
+      allow(game_io).to receive(:get_input) { "5" }
+      allow(game_io).to receive(:puts_message) { }
       expect(test_player.play_move(state)).to eq(5)
     end
 
     it "accpets 6 when that is a valid move " do
       state = GameState.new(board, test_player, test_player2)
-      allow(game_io.wrapper).to receive(:gets) { "6\n" }
+      allow(game_io).to receive(:get_input) { "6" }
+      allow(game_io).to receive(:puts_message) { }
       expect(test_player.play_move(state)).to eq(6)
     end
 
     it "accpets 7 when that is a valid move " do
       state = GameState.new(board, test_player, test_player2)
-      allow(game_io.wrapper).to receive(:gets) { "7\n" }
+      allow(game_io).to receive(:get_input) { "7" }
+      allow(game_io).to receive(:puts_message) { }
       expect(test_player.play_move(state)).to eq(7)
     end
 
     it "accpets 8 when that is a valid move " do
       state = GameState.new(board, test_player, test_player2)
-      allow(game_io.wrapper).to receive(:gets) { "8\n" }
+      allow(game_io).to receive(:get_input) { "8" }
+      allow(game_io).to receive(:puts_message) { }
       expect(test_player.play_move(state)).to eq(8)
-    end
-     
-    it "rejects 5 and takes second input of 1 when that is a valid move " do
-      board.spaces[5] = 'X'
-      state = GameState.new(board, test_player, test_player2)
-      allow(game_io.wrapper).to receive(:gets) { "5\n" }
-      allow(game_io.wrapper).to receive(:gets) { "1\n" }
-      should_return =  "Please input a move: "
-      expect(STDOUT).to receive(:puts).with(should_return)
-      should_return =  "That is not a valid move, please try again : "
-      expect(STDOUT).to receive(:puts).with(should_return)
-
-      expect(test_player.play_move(state)).to eq(1)
     end
 
   end
