4ba94f5ffdf15689d938b3fee3e35f39933bf0ae
Tyler Decker <tyler.decker@gmail.com>
2016-04-25 13:59:11 -0500

    Removed switch_turns method.

--- lib/game_state.rb
+++ lib/game_state.rb
@@ -6,12 +6,6 @@ class GameState
     @opponent = opponent_mark
   end
 
-  def switch_turns
-    temp = @player
-    @player = @opponent
-    @opponent = @player
-  end
-
   def deep_copy
     temp_board = game_board.deep_copy
     GameState.new(temp_board, @player, @opponent)
--- spec/game_state_spec.rb
+++ spec/game_state_spec.rb
@@ -9,12 +9,4 @@ describe GameState do
       test_board.spaces[0] = 'X'
       expect(test_state.game_board.spaces[0]).to eq(' ')
   end
-
-  it "Swaps players " do
-    test_state = GameState.new(test_board, 'X', 'O')
-    test_state.switch_turns
-    expect(test_state.player).to eq('O')
-  end
-
-
 end
