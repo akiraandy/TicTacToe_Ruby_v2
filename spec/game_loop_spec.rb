6fc31c1ccadf86f9cc7fe704849d53be3e20f40c
Tyler Decker <tyler.decker@gmail.com>
2016-04-25 20:55:21 -0500

    Updated test.

--- spec/game_loop_spec.rb
+++ spec/game_loop_spec.rb
@@ -19,15 +19,12 @@ describe GameLoop do
       test_loop.game_loop
     end
 
-    # todo :: figure out how to make this test fail
-    # not sure how to have it check the internal loop and make sure that
-    # it is not printing the end of game screen.
     it "doesn't print end of screen if @rules.game_over? == false " do
-      allow(rules).to receive(:game_over?) { false }
+      allow(rules).to receive(:game_over?).and_return(false, true)
       allow(game_screen).to receive(:print_screen) { }
       expect(players).to receive(:play_turn)
+      expect(players).to receive(:play_turn)
       expect(game_screen).to receive(:print_end_of_game)
-      allow(rules).to receive(:game_over?) { true }
       test_loop.game_loop
     end
   end
