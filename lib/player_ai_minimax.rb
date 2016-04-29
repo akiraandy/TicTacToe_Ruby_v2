4dcd6580e3b5de7ab0634b265e30d75313c1d996
Tyler Decker <tyler.decker@gmail.com>
2016-04-29 09:10:59 -0500

    Created PlayerAiMiniax class.

--- lib/player_ai_minimax.rb
+++ lib/player_ai_minimax.rb
@@ -7,18 +7,51 @@ class PlayerAiMinimax
   end
 
   def play_move(game_state)
-    @board = game_state.game_board
-    @spaces = @board.spaces
-    @available_moves = @board.available_moves
-    @player = game_state.player
-    @opponent = game_state.opponent
-    
-    if (@available_moves.size == 1) 
-      return @available_moves[0]
-    end
-    @available_moves.size
+    board = game_state.game_board
+    return 0 if board.empty?
+    return 4 if opponent_played_corner_for_first_move(board)
+    @current_player = game_state.player
+    @non_current_player = game_state.opponent
+    minimax(board, game_state.player, game_state.opponent)
+    @choice
   end
 
+  private
+  def opponent_played_corner_for_first_move(board)
+    corners = [0,2,6,8]
+    if (board.available_moves.size == 8)
+      corners.each do |i|
+        return true if board.spaces[i] != ' '
+      end
+    end
+    false
+  end
   
+  def score(board)
+    return 10 if @rules.winner(board) == @current_player
+    return -10 if @rules.winner(board) == @non_current_player
+    return 0
+  end
+
+  def minimax(board, player, opponent)
+    return score(board) if @rules.game_over?(board)
+    scores = []
+    moves = []
+    board.available_moves.each do |move|
+      possible_game_board = board.deep_copy
+      possible_game_board.spaces[move] = player
+      scores.push minimax(possible_game_board, opponent, player)
+      moves.push move
+    end
+    if player == @current_player
+      max_score_index = scores.each_with_index.max[1]
+      @choice = moves[max_score_index]
+      return scores[max_score_index]
+    else
+      min_score_index = scores.each_with_index.min[1]
+      @choice = moves[min_score_index]
+      return scores[min_score_index]
+    end
+  end
 end
 
--- spec/player_ai_minimax_spec.rb
+++ spec/player_ai_minimax_spec.rb
@@ -9,35 +9,104 @@ describe PlayerAiMinimax do
   let (:board) { GameBoard.new }
   let (:rules) { GameRules.new }
 
-  let (:test_ai) { PlayerAiMinimax.new(:O, rules) }
-  let (:test_ai2) { PlayerAiMinimax.new(:X, rules) }
+  let (:test_ai) { PlayerAiMinimax.new(:X, rules) }
+  let (:test_ai2) { PlayerAiMinimax.new(:O, rules) }
   let (:test_state) { GameState.new(board , :X, :O) }
   let(:spaces) { test_state.game_board.spaces }
   let(:corners) { [0, 2, 6, 8] }
 
 
   describe "It has a mark " do
-    it ".mark returns :O " do
-      expect(test_ai.mark).to eq(:O)    
+    it ".mark returns :X " do
+      expect(test_ai.mark).to eq(:X)
 
     end
   end
   
-  describe "Playing a move " do
+  describe "Playing the last move " do
     it "plays 0 when that is the only space open " do
       [1,2,3,4,5,6,7,8].each do |i|
-        spaces[i] = :T
+        spaces[i] = i
       end
       expect(test_ai.play_move(test_state)).to eq(0)
     end
     it "plays 1 when that is the only space open " do
       [0,2,3,4,5,6,7,8].each do |i|
-        spaces[i] = :T
+        spaces[i] = i
       end
       expect(test_ai.play_move(test_state)).to eq(1)
     end
- 
   end
 
+  describe "Playing second to last move " do
+    it "when only 7 and 8 are left it plays 7 when that will block opponent and 8 will not " do
+      [1,2,3,4].each do |i|
+        spaces[i] = :X
+      end
+      [0,5,6].each do |i|
+        spaces[i] = :O
+      end
+      expect(test_ai.play_move(test_state)).to eq(7)
+    end
 
+    it "when only 6 and 7 are left it plays 7 and neither move wins / blocks " do
+      [1,2,3,8].each do |i|
+        spaces[i] = :X
+      end
+      [0,4,5].each do |i|
+        spaces[i] = :O
+      end
+      expect(test_ai.play_move(test_state)).to eq(7)
+    end
+
+    it "when only 1 and 2 are left it plays 2 and neither move wins / blocks " do
+      [0,4,5,6].each do |i|
+        spaces[i] = :X
+      end
+      [3,7,8].each do |i|
+        spaces[i] = :O
+      end
+      expect(test_ai.play_move(test_state)).to eq(2)
+    end
+  end
+
+  describe "When it goes first " do
+    it "plays 0 on empty board " do
+      expect(test_ai.play_move(test_state)).to eq(0)
+    end
+  end
+
+  describe "When it goes second " do
+    it "plays center square if other player played a corner 0" do
+      spaces[0] = :O
+      expect(test_ai.play_move(test_state)).to eq(4)
+    end
+
+    it "plays center square if other player played a corner 2" do
+      spaces[2] = :O
+      expect(test_ai.play_move(test_state)).to eq(4)
+    end
+
+    it "plays center square if other player played a corner 6" do
+      spaces[6] = :O
+      expect(test_ai.play_move(test_state)).to eq(4)
+    end
+
+    it "plays center square if other player played a corner 8" do
+      spaces[8] = :O
+      expect(test_ai.play_move(test_state)).to eq(4)
+    end
+  end
+
+  describe "Creating a Fork " do
+    it "plays 2 to create a fork " do
+      [0,6].each do |i|
+        spaces[i] = :X
+      end
+      [8,3].each do |i|
+        spaces[i] = :O
+      end
+      expect(test_ai.play_move(test_state)).to eq(2)
+    end
+  end
 end
