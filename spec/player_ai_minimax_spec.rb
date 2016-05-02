eed11d8d850ca5a781c669168b97daaebd455d18
Tyler Decker <tyler.decker@gmail.com>
2016-05-02 11:55:43 -0500

    Cleaned up tests & added new Tests.
    Made a helper function for playing mulitple moves on the board, as this
    was used in the setup of several tests.
    Added tests to see if ai goes second and player 1 played the center or a
    side move.

--- spec/player_ai_minimax_spec.rb
+++ spec/player_ai_minimax_spec.rb
@@ -4,7 +4,6 @@ require 'game_state'
 require 'game_rules'
 
 describe PlayerAiMinimax do
-  let (:io) { double }
   let (:board) { GameBoard.new }
   let (:rules) { GameRules.new }
   let (:test_ai) { PlayerAiMinimax.new(:X, rules) }
@@ -21,7 +20,7 @@ describe PlayerAiMinimax do
   
   describe "Playing the last move " do
     it "plays 0 when that is the only space open " do
-      [1,2,3,4,5,6,7,8].each do |i|
+      (1..8).each do |i|
         spaces[i] = i
       end
       expect(test_ai.play_move(test_state)).to eq(0)
@@ -35,33 +34,21 @@ describe PlayerAiMinimax do
   end
 
   describe "Playing second to last move " do
-    it "when only 7 and 8 are left it plays 7 when that will block opponent and 8 will not " do
-      [1,2,3,4].each do |i|
-        spaces[i] = :X
-      end
-      [0,5,6].each do |i|
-        spaces[i] = :O
-      end
+    it "it will play to block if the other move doesn't block or win." do
+      set_spaces(:X, [1,2,3,4])
+      set_spaces(:O, [0,5,6])
       expect(test_ai.play_move(test_state)).to eq(7)
     end
 
     it "when only 6 and 7 are left it plays 7 and neither move wins / blocks " do
-      [1,2,3,8].each do |i|
-        spaces[i] = :X
-      end
-      [0,4,5].each do |i|
-        spaces[i] = :O
-      end
+      set_spaces(:X, [1,2,3,8])
+      set_spaces(:O, [0,4,5])
       expect(test_ai.play_move(test_state)).to eq(7)
     end
 
     it "when only 1 and 2 are left it plays 2 and neither move wins / blocks " do
-      [0,4,5,6].each do |i|
-        spaces[i] = :X
-      end
-      [3,7,8].each do |i|
-        spaces[i] = :O
-      end
+      set_spaces(:X, [0,4,5,6])
+      set_spaces(:O, [3,7,8])
       expect(test_ai.play_move(test_state)).to eq(2)
     end
   end
@@ -73,36 +60,74 @@ describe PlayerAiMinimax do
   end
 
   describe "When it goes second " do
-    it "plays center square if other player played a corner 0" do
-      spaces[0] = :O
-      expect(test_ai.play_move(test_state)).to eq(4)
+    describe "Player 1 played a corner " do
+      it "plays center square if other player played a corner 0" do
+        spaces[0] = :O
+        expect(test_ai.play_move(test_state)).to eq(4)
+      end
+
+      it "plays center square if other player played a corner 2" do
+        spaces[2] = :O
+        expect(test_ai.play_move(test_state)).to eq(4)
+      end
+
+      it "plays center square if other player played a corner 6" do
+        spaces[6] = :O
+        expect(test_ai.play_move(test_state)).to eq(4)
+      end
+
+      it "plays center square if other player played a corner 8" do
+        spaces[8] = :O
+        expect(test_ai.play_move(test_state)).to eq(4)
+      end
     end
 
-    it "plays center square if other player played a corner 2" do
-      spaces[2] = :O
-      expect(test_ai.play_move(test_state)).to eq(4)
-    end
+    describe "Player 1 played center " do
+      it "returns a corner move " do
+        set_spaces(:O, [4])
+        move = test_ai.play_move(test_state)
+        expect(corners.include?(move)).to eq(true)
+      end
+    end 
 
-    it "plays center square if other player played a corner 6" do
-      spaces[6] = :O
-      expect(test_ai.play_move(test_state)).to eq(4)
-    end
+    describe "Player 1 played a side " do
+      it "returns a cornver move if player1 played 1 " do
+        set_spaces(:O, [1])
+        move = test_ai.play_move(test_state)
+        expect(corners.include?(move)).to eq(true)
+      end
 
-    it "plays center square if other player played a corner 8" do
-      spaces[8] = :O
-      expect(test_ai.play_move(test_state)).to eq(4)
+      it "returns a cornver move if player1 played 3 " do
+        set_spaces(:O, [3])
+        move = test_ai.play_move(test_state)
+        expect(corners.include?(move)).to eq(true)
+      end
+
+      it "returns a cornver move if player1 played 5 " do
+        set_spaces(:O, [5])
+        move = test_ai.play_move(test_state)
+        expect(corners.include?(move)).to eq(true)
+      end
+
+      it "returns a cornver move if player1 played 7 " do
+        set_spaces(:O, [7])
+        move = test_ai.play_move(test_state)
+        expect(corners.include?(move)).to eq(true)
+      end
+
+
     end
   end
 
   describe "Creating a Fork " do
     it "plays 2 to create a fork " do
-      [0,6].each do |i|
-        spaces[i] = :X
-      end
-      [8,3].each do |i|
-        spaces[i] = :O
-      end
-      expect(test_ai.play_move(test_state)).to eq(2)
+     set_spaces(:X, [0, 6])
+     set_spaces(:O, [3, 8])
+     expect(test_ai.play_move(test_state)).to eq(2)
     end
   end
+  
+  def set_spaces(mark, spaces_to_set)
+    spaces_to_set.each { |i| spaces[i] = mark }
+  end  
 end
