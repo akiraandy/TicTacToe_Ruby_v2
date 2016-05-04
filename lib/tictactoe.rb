0d98d6adff689942d62a7a0e0f94cdb3b0db994a
Tyler Decker <tyler.decker@gmail.com>
2016-05-04 16:20:14 -0500

    Changed TicTacToe class to be an API.
    It is no longer a command line runner for a TicTacToe Game. Instead it
    is an API the talks to the other classes.

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,22 +1,78 @@
+require 'ai_basic'
+require 'ai_minimax'
 require 'game_board'
 require 'game_rules'
+require 'game_state'
 require 'player_manager'
-require 'game_loop'
-require 'game_screen'
+
 class TicTacToe
 
-  attr_accessor :board, :game_loop
-
-  def initialize ()
-
+  def initialize()
+    @rules = GameRules.new
+    @board = GameBoard.new
+    @players = PlayerManager.new
+    @ai_easy = AiBasic.new(@rules)
+    @ai_hard = AiMinimax.new(@rules)
   end
- 
+
   def new_game
     @board = GameBoard.new
   end
 
-  def game_over?
-
+  def valid_move?(move)
+    @board.valid_move?(move)
   end
- 
+
+  def get_board
+    @board.spaces
+  end
+
+  def game_over?
+    @rules.game_over?(@board)
+  end
+
+  def tied_game?
+    @rules.tied?(@board)
+  end
+
+  def winner
+    @rules.winner(@board)
+  end
+
+  def play_human_move(move)
+    if @board.valid_move?(move)
+      @board.spaces[move] = @players.current_player.mark
+      @players.switch_turns
+    end
+  end
+
+  def get_ai_player_move
+    @players.current_player.type.get_move(get_game_state) if is_current_player_ai?
+  end
+
+  def set_player1_ai_easy
+    @players.set_player1_type(@ai_easy)
+  end
+
+  def set_player2_ai_easy
+    @players.set_player2_type(@ai_easy)
+  end
+
+  def set_player1_ai_hard
+    @players.set_player1_type(@ai_hard)
+  end
+
+  def set_player2_ai_hard
+    @players.set_player2_type(@ai_hard)
+  end
+
+  private
+
+  def is_current_player_ai?
+    @players.current_player.type.respond_to?(:get_move)
+  end
+
+  def get_game_state
+    GameState.new(@board, @players.current_player.mark, @players.non_current_player.mark)
+  end
 end
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,31 +1,130 @@
 require 'tictactoe'
+require 'ai_basic'
+require 'game_rules'
 
 describe TicTacToe do
-  let (:game_io) { double }
-  let (:player1) { double }
-  let (:player2) { double }
-  let (:test_ttt) { TicTacToe.new(game_io, player1, player2) }
+  let(:rules) { double }
+  let(:board) { double }
+  let(:players) { double }
+  let(:state) { double }
+  let(:ai_basic) { AiBasic.new(GameRules.new) }
+  Player = Struct.new(:mark, :type)
+  let(:test_player) { Player.new(:X, ai_basic) }
 
-  it "runs the main loop " do
-    expect(test_ttt.game_loop).to receive(:game_loop).once
-    expect(game_io).to receive(:puts_message)
-    expect(game_io).to receive(:get_input).and_return("N")
-    test_ttt.play_game
+  describe "making a new game" do
+    it "@board is empty after call to new_game" do
+      empty_board = []
+      (9).times do
+        empty_board.push(' ')
+      end
+      subject.play_human_move(0)
+      subject.new_game
+      expect(subject.get_board).to eq(empty_board)
+    end
   end
 
-  it "plays the game twice " do
-    expect(test_ttt.game_loop).to receive(:game_loop).twice
-    allow(test_ttt).to receive(:new_game)
-    expect(game_io).to receive(:puts_message).twice
-    expect(game_io).to receive(:get_input).and_return("Y", "N")
-    test_ttt.play_game
+  describe "boolean methods : game_over? tied? valid_move?" do
+    it "returns false when game is not over" do
+      subject.instance_variable_set("@rules", rules)
+      expect(rules).to receive(:game_over?)
+      subject.game_over?
+    end
+
+    it "tied? calls rules.tied?" do
+      subject.instance_variable_set("@rules", rules)
+      expect(rules).to receive(:tied?)
+      subject.tied_game?
+    end
+
+    it "valid_move? calls board.valid_move?" do
+      subject.instance_variable_set("@board", board)
+      expect(board).to receive(:valid_move?).with(0)
+      subject.valid_move?(0)
+    end
   end
 
-  it "gameboad is blank after new_game " do
-    expect(test_ttt.game_loop).to receive(:game_loop).twice
-    expect(test_ttt).to receive(:play_again?).and_return(true, false)
-    test_ttt.board.spaces[0] = 'X'
-    test_ttt.play_game
-    expect(test_ttt.board.spaces[0]).to eq(' ')
+  describe "Getting the winner" do
+    it "winner calls rules.winner(@board)" do
+      subject.instance_variable_set("@rules", rules)
+      expect(rules).to receive(:winner)
+      subject.winner
+    end
+  end
+
+
+  describe "Getting the game_board" do
+    it "get_board calls @board.spaces" do
+      subject.instance_variable_set("@board", board)
+      expect(board).to receive(:spaces)
+      subject.get_board
+    end
+  end
+
+  describe "Playing a Human move" do
+    it "if move is valid @board.spaces and @player.switch_turns are called" do
+      subject.instance_variable_set("@players", players)
+      subject.instance_variable_set("@board", board)
+      expect(board).to receive(:valid_move?).with(0).and_return(true)
+      expect(board).to receive(:spaces)
+      expect(players).to receive(:switch_turns)
+      subject.play_human_move(0)
+    end
+
+    it "if move is not valid @board.spaces and @player.switch_turns are NOT called" do
+      subject.instance_variable_set("@board", board)
+      subject.instance_variable_set("@players", players)
+      expect(board).to receive(:valid_move?).with(0).and_return(false)
+      expect(board).to receive(:spaces).exactly(0).times
+      expect(players).to receive(:switch_turns).exactly(0).times
+      subject.play_human_move(0)
+    end
+  end
+
+  describe "Getting an Ai move" do
+    it "current_player is an Ai it calls get_move" do
+      subject.instance_variable_set("@players", players)
+      expect(subject).to receive(:get_game_state)
+      expect(subject).to receive(:is_current_player_ai?).and_return(true)
+      expect(players).to receive(:current_player).and_return(test_player)
+      expect(test_player).to receive(:type).and_return(ai_basic)
+      expect(ai_basic).to receive(:get_move)
+      subject.get_ai_player_move
+    end
+
+    it "current_player is NOT Ai it doesn't call get_move" do
+      subject.instance_variable_set("@players", players)
+      expect(subject).to receive(:is_current_player_ai?).and_return(false)
+      expect(subject).to receive(:get_game_state).exactly(0).times
+      expect(players).to receive(:current_player).and_return(test_player).exactly(0).times
+      expect(test_player).to receive(:type).and_return(ai_basic).exactly(0).times
+      expect(ai_basic).to receive(:get_move).exactly(0).times
+      subject.get_ai_player_move
+    end
+  end
+
+  describe "Setting player's type" do
+    it "set_player1_ai_easy calls @players.set_player1_type" do
+      subject.instance_variable_set("@players", players)
+      expect(players).to receive(:set_player1_type)
+      subject.set_player1_ai_easy
+    end
+
+    it "set_player1_ai_hard calls @players.set_player1_type" do
+      subject.instance_variable_set("@players", players)
+      expect(players).to receive(:set_player1_type)
+      subject.set_player1_ai_hard
+    end
+
+    it "set_player2_ai_easy calls @players.set_player2_type" do
+      subject.instance_variable_set("@players", players)
+      expect(players).to receive(:set_player2_type)
+      subject.set_player2_ai_easy
+    end
+
+    it "set_player2_ai_hard calls @players.set_player2_type" do
+      subject.instance_variable_set("@players", players)
+      expect(players).to receive(:set_player2_type)
+      subject.set_player2_ai_hard
+    end
   end
 end
