1e0eac8f5421ee804c9a46faa9278fd723c815f1
Tyler Decker <tyler.decker@gmail.com>
2016-05-31 15:18:20 -0500

    Removed instance variable setting.

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,12 +1,14 @@
 require 'game_board'
 require 'game_rules'
 require 'game_state'
+require 'player_manager'
 
 class TicTacToe
 
-  def initialize(board, rules)
+  def initialize(player_manager, board, rules)
     @rules = rules
     @board = board
+    @players = player_manager
   end
 
   def get_board
@@ -31,12 +33,21 @@ class TicTacToe
 
   def play_move(move)
     if valid_move?(move)
-      @board.play_move(move)
+      @board.play_move(@players.current_player.mark, move)
+      @players.switch_turns
     end
   end
 
   def valid_move?(move)
     @board.valid_move?(move)
+  end
+
+  def get_ai_player_move
+    @players.current_player.type.get_move(get_game_state) if is_current_player_ai?
+  end
+
+  def is_current_player_ai?
+    @players.current_player.type.respond_to?(:get_move)
   end
 
   private
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -7,8 +7,10 @@ describe TicTacToe do
   let(:board) { double }
   let(:players) { double }
   let(:state) { double }
-
-  let(:test_ttt) { TicTacToe.new(board, rules) }
+  let(:ai_basic) { AiBasic.new(GameRules.new) }
+  Player = Struct.new(:mark, :type)
+  let(:test_player) { Player.new(:X, ai_basic) }
+  let(:test_ttt) { TicTacToe.new(players, board, rules) }
 
   describe "boolean methods : game_over? tied? valid_move?" do
     it "returns false when game is not over" do
@@ -46,6 +48,8 @@ describe TicTacToe do
     it "if move is valid @board.spaces and @player.switch_turns are called" do
       expect(board).to receive(:valid_move?).with(0).and_return(true)
       expect(board).to receive(:play_move)
+      allow(players).to receive(:current_player).and_return(test_player)
+      expect(players).to receive(:switch_turns)
       test_ttt.play_move(0)
     end
 
@@ -54,6 +58,26 @@ describe TicTacToe do
       expect(board).to receive(:spaces).exactly(0).times
       expect(players).to receive(:switch_turns).exactly(0).times
       test_ttt.play_move(0)
+    end
+  end
+
+  describe "Getting an Ai move" do
+    it "current_player is an Ai it calls get_move" do
+      expect(test_ttt).to receive(:get_game_state)
+      expect(test_ttt).to receive(:is_current_player_ai?).and_return(true)
+      expect(players).to receive(:current_player).and_return(test_player)
+      expect(test_player).to receive(:type).and_return(ai_basic)
+      expect(ai_basic).to receive(:get_move)
+      test_ttt.get_ai_player_move
+    end
+
+    it "current_player is NOT Ai it doesn't call get_move" do
+      expect(test_ttt).to receive(:is_current_player_ai?).and_return(false)
+      expect(test_ttt).to receive(:get_game_state).exactly(0).times
+      expect(players).to receive(:current_player).and_return(test_player).exactly(0).times
+      expect(test_player).to receive(:type).and_return(ai_basic).exactly(0).times
+      expect(ai_basic).to receive(:get_move).exactly(0).times
+      test_ttt.get_ai_player_move
     end
   end
 end
