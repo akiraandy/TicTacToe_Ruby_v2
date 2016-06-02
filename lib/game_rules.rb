e047b9e3e4a44a4a59462c41b05afff1d02bb2d8
Tyler Decker <tyler.decker@gmail.com>
2016-06-02 13:27:23 -0500

    Added loop to reduce code duplication

--- lib/game_rules.rb
+++ lib/game_rules.rb
@@ -15,21 +15,17 @@ class GameRules
 private
 
   def check_win_col(spaces)
-    return spaces[0] if [spaces[0], spaces[3], spaces[6]].uniq.length == 1 && \
-      spaces[0] != ' '
-    return spaces[1] if [spaces[1], spaces[4], spaces[7]].uniq.length == 1 && \
-      spaces[1] != ' '
-    return spaces[2] if [spaces[2], spaces[5], spaces[8]].uniq.length == 1 && \
-      spaces[2] != ' '
+    [0,1,2].each do |i|
+    return spaces[i] if [spaces[i], spaces[i + 3], spaces[i + 6]].uniq.length == 1 && \
+      spaces[i] != ' '
+    end
   end
 
   def check_win_row(spaces)
-    return spaces[0] if [spaces[0], spaces[1], spaces[2]].uniq.length == 1 && \
-      spaces[0] != ' '
-    return spaces[3] if [spaces[3], spaces[4], spaces[5]].uniq.length == 1 && \
-      spaces[3] != ' '
-    return spaces[6] if [spaces[6], spaces[7], spaces[8]].uniq.length == 1 && \
-      spaces[6] != ' '
+    [0, 3, 6].each do |i|
+    return spaces[i] if [spaces[i], spaces[i + 1], spaces[i + 2]].uniq.length == 1 && \
+      spaces[i] != ' '
+    end
   end
 
   def check_win_diagonal(spaces)
